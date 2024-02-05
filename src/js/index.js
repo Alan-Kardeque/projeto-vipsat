013bbb486dd39522bfb5b9b69c03721aadc30cef
Alan-Kardeque <146044069+Alan-Kardeque@users.noreply.github.com>
2024-02-04 22:51:42 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,46 @@ elementosDuvidas.forEach(function (duvida) {
         duvida.classList.toggle('ativa')
     })
 })
+
+class MobileNavbar {
+    constructor(mobileMemu, navList, navLinks) {
+        this.mobileMemu = document.querySelector(mobileMemu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMemu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.mobileMemu.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        if (this.mobileMemu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+".mobile-menu",
+".nav-list",
+".nav-list li",
+);
+mobileNavbar.init();
